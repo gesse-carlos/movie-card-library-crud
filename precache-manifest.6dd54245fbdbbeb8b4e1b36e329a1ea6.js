f4f77eebaa0dff60cb696fb58e76bf3f8646992a
gesse-carlos <gesse-carlos@users.noreply.github.com>
2021-09-08 08:07:27 +0000

    deploy: c6d23319c5eb494fa50b2b6aec0f7dad7b92bbad

---
+++ precache-manifest.6dd54245fbdbbeb8b4e1b36e329a1ea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b2c59512fa78d82cbc68fed72ef1ed",
+    "url": "/movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "f8b57d6bce8cd248f1c9",
+    "url": "/movie-card-library-crud/static/css/main.7f51c7e4.chunk.css"
+  },
+  {
+    "revision": "10036fbd6b59e6a2880a",
+    "url": "/movie-card-library-crud/static/js/2.faad82ee.chunk.js"
+  },
+  {
+    "revision": "f8b57d6bce8cd248f1c9",
+    "url": "/movie-card-library-crud/static/js/main.85552914.chunk.js"
+  },
+  {
+    "revision": "636dadd53749eb2456d4",
+    "url": "/movie-card-library-crud/static/js/runtime-main.7b328ff8.js"
+  }
+]);
